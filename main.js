4866cffce99ade668520d482a2b15eb78af66225
Jhonny <apenas.um.qu4lquer@gmail.com>
2024-04-02 16:26:35 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 const botoes=document.querySelectorAll(".botao");
-for(let i=0; i <botoes.length;i++) {
-    botoes[i].onclick= function() 
-    
-    {
-    for(let j=0; j<botoes.length;j++){
+const textos=document.querySelectorAll(".aba-conteudo");
+const contadores=document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-04-25")
+contadores[0].textContent= tempoObjetivo1
+
+   for(let i=0; i<botoes.length;i++) {
+   botoes[i].onclick=function() {
+    for(let  j=0;j<botoes.length;j++){
         botoes[j].classList.remove("ativo");
+        textos[j].classList.remove("ativo");
     }
-    botoes [i].classList.add("ativo");
-    }
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    
+   }
 }
