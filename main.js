dc20e8ed3c571bfac8fc28abdf57ab22bf68f258
Jhonny <apenas.um.qu4lquer@gmail.com>
2024-04-16 15:07:06 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
-const botoes=document.querySelectorAll(".botao");
-const textos=document.querySelectorAll(".aba-conteudo");
-const contadores=document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-04-25")
-contadores[0].textContent= tempoObjetivo1
+const botoes = document.querySelectorAll(".botao");
+const textos= document.querySelectorAll(".aba-conteudo");
+const contadores= document.querySelectorAll(".contador");
+const tempoObjetivo1= new Date("2024-04-25");
+contadores[0].textContent =tempoObjetivo1;
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function(){
 
-   for(let i=0; i<botoes.length;i++) {
-   botoes[i].onclick=function() {
-    for(let  j=0;j<botoes.length;j++){
-        botoes[j].classList.remove("ativo");
-        textos[j].classList.remove("ativo");
-    }
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+
+        }
+
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
-    
-   }
+
+    }
 }
