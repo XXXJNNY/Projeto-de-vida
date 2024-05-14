2cc5d6f15d346ec5931f00d2338c52f5567b8e0c
Jhonny <apenas.um.qu4lquer@gmail.com>
2024-05-14 16:09:00 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-07-03T00:00:00");
+const tempoObjetivo1 = new Date("2007-07-03T00:00:00");
 const tempoObjetivo2 = new Date("2024-08-03T00:00:00");
-const tempoObjetivo3 = new Date("2024-09-03T00:00:00");
-const tempoObjetivo4 = new Date("2024-10-03T00:00:00");
+const tempoObjetivo3 = new Date("2043-09-03T00:00:00");
+const tempoObjetivo4 = new Date("2024-05-23T00:00:00");
 
 const tempos=[tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4]
 
@@ -43,6 +43,23 @@ function calculaTempo(tempoObjetivo) {
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
- 
+    if(tempoFinal>0){
  return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    } else{
+        return "𝒇𝒊𝒏𝒂𝒍𝒊𝒛𝒂𝒅𝒐"
+    }
+
 }
+
+function atualizaCronometro(){
+     for (let i=0; i<contadores.length;i++){
+      contadores[i].textContent = calculaTempo(tempos[i])
+     }
+}
+
+function comecaCronometro(){
+     atualizaCronometro();
+     setInterval(atualizaCronometro,1000);
+
+}
+comecaCronometro();
